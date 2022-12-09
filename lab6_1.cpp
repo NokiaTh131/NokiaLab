29d34016681c321abefc7f19e5a6aa8441aa1f29
NokiaTh131 <eho55120@gmail.com>
2022-12-09 16:33:44 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,7 +2,22 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
+
+    int i = 0;
+    int num_source[];
+    int N = 0;
+ 
+    while(i < 1) {
+        cout << "Enter an integer: ";
+        cin >> num;
+        num = num_source[N]
+        N++;
+
+        if(num == 0) {
+            i++;
+        }
+    }
+
     cout << "#Even numbers = ";
     cout << "#Odd numbers = ";
     return 0;
